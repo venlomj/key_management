f9557337ebe64dd86abf63c0e5aacc0a58d4e5ba
venlomj <venlo.mj@hotmail.nl>
2024-12-16 14:42:44 +0100

    Style adjustments.

--- resources/views/components/layout/topbar.blade.php
+++ resources/views/components/layout/topbar.blade.php
@@ -1,12 +1,12 @@
-<nav class="shadow bg-white/70 sticky top-0 backdrop-blur-sm z-10 mb-4 flex justify-end items-center p-4">
+<nav class="shadow bg-white/70 sticky top-0 backdrop-blur-sm z-10 mb-4 flex justify-end items-center p-4 pr-6">
     <div class="flex items-center space-x-4">
-        <x-nav-link href="{{ route('admin.persons') }}">
+        <x-nav-link href="{{ route('admin.persons') }}" class="text-sidebar-dark-light">
             Personen
         </x-nav-link>
-        <x-nav-link href="{{ route('admin.keys') }}">
+        <x-nav-link href="{{ route('admin.keys') }}" class="text-sidebar-dark-light">
             Sleutels
         </x-nav-link>
-        <x-nav-link href="{{ route('admin.classrooms') }}">
+        <x-nav-link href="{{ route('admin.classrooms') }}" class="text-sidebar-dark-light">
             Lokalen
         </x-nav-link>
     </div>
